7074e31bd1ae5d9b19c748a70a6b859747ac5c58
Felip3 <juanfeliperamirezcardona0@gmail.com>
2025-05-28 16:19:05 -0500

    Update ContenedorPrincipal.jsx

--- src/pages/ContenedorPrincipal.jsx
+++ src/pages/ContenedorPrincipal.jsx
@@ -2,7 +2,7 @@ import './ContenedorPrincipal.css'
 import Cabecera from "../components/Cabecera"
 import Tarjeta from '../components/Tarjeta'
 import PiePagina from '../components/PiePagina'
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 let apiUsuarios = "https://back-json-server-tuya.onrender.com/propiedades";
 
 function ContenedorPrincipal(){
@@ -15,12 +15,16 @@ function ContenedorPrincipal(){
             .catch((error) => console.log(error));
 
     }
+    useEffect(() => {
+        buscarPropiedades();
+    }, []);
+
     
     return(
         <div className="contenedorPrincipal">
             <Cabecera></Cabecera>
             <main className='contenedorTarjetas'>
-                <h1 onLoad={buscarPropiedades()} >Explora alojamientos cerca de ti</h1>
+                <h1 >Explora alojamientos cerca de ti</h1>
                 {
                     propiedades.map ((propiedad)=>{
                         return <Tarjeta info={propiedad}></Tarjeta>
